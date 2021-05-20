ca80e9825c05a48f57eee7b48e3e13c3b66833a0
OfirGilad <ofirgila@post.bgu.ac.il>
2021-05-20 16:59:24 +0300

    starting 2a still not finished

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import matplotlib.pyplot as plt
 
 
 def ex2a():
-    G, x = generate_experiment()
+    G, x, y = generate_experiment()
     lambda_x = 80
-    Gx = G @ x
-    result = (lambda_x / 2) * pow((np.linalg.norm(Gx, 2)), 2)
-    print("result: \n %s" % result)
+
+    # Since in our case: A = I, we get that:
+    # x = (I + (lambda_x/2) * GtG)^-1 * y
+    # mark: z = (I + (lambda_x/2) * GtG)^-1 , so: x = z * y
+    I = np.eye(np.size(x))
+    GtG = G.transpose() @ G
+    z = np.invert(np.array(I + ((lambda_x / 2) * GtG), dtype=int))
+    x_result = z @ y
+    print("x: \n %s" % x_result)
+    plt.plot(x, x_result)
+    plt.show()
 
 
 def generate_experiment():
@@ -18,7 +26,7 @@ def generate_experiment():
     f = np.zeros(x.shape)
     f[0: one] = 0.0 + 0.5 * x[0: one]
     f[one: (2 * one)] = 0.8 - 0.2 * np.log(x[100: 200])
-    f[(2 * one): (3 * one)] = 0.7 - 0.3 * x[(2 * one): 3 * one]
+    f[(2 * one): (3 * one)] = 0.7 - 0.2 * x[(2 * one): 3 * one]
     f[(3 * one): (4 * one)] = 0.3
     f[(4 * one): (5 * one)] = 0.5 - 0.1 * x[(4 * one): (5 * one)]
     G = spdiags([-np.ones(n), np.ones(n)], np.array([0, 1]), n - 1, n).toarray()
@@ -28,7 +36,7 @@ def generate_experiment():
     plt.plot(x, y)
     plt.plot(x, f)
     plt.show()
-    return G, x
+    return G, x, y
 
 
 def ex4a():
