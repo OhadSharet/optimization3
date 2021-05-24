21242c30e5b3fead18155800309f4ec8cb97a995
OfirGilad <ofirgila@post.bgu.ac.il>
2021-05-24 12:19:37 +0300

    4c MNIST working

--- loadMNIST.py
+++ loadMNIST.py
@@ -61,11 +61,11 @@ class MnistDataloader(object):
 # Set file paths based on added MNIST Datasets
 #
 cwd = os.getcwd()
-input_path = cwd + '\MNIST'
-training_images_filepath = join(input_path, 'train-images-idx3-ubyte\\train-images.idx3-ubyte')
-training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte\\train-labels.idx1-ubyte')
-test_images_filepath = join(input_path, 't10k-images-idx3-ubyte\\t10k-images.idx3-ubyte')
-test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte\\t10k-labels.idx1-ubyte')
+input_path = cwd + '/MNIST'
+training_images_filepath = join(input_path, 'train-images.idx3-ubyte')
+training_labels_filepath = join(input_path, 'train-labels.idx1-ubyte')
+test_images_filepath = join(input_path, 't10k-images.idx3-ubyte')
+test_labels_filepath = join(input_path, 't10k-labels.idx1-ubyte')
 
 
 #
@@ -90,23 +90,28 @@ def show_images(images, title_texts):
 #
 # Load MINST dataset
 #
-mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath,
-                                   test_labels_filepath)
-(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+def loadMNIST_main():
+    mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath,
+                                       test_labels_filepath)
+    (x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
 
-#
-# Show some random training and test images
-#
-images_2_show = []
-titles_2_show = []
-for i in range(0, 10):
-    r = random.randint(1, 60000)
-    images_2_show.append(x_train[r])
-    titles_2_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))
+    #
+    # Show some random training and test images
+    #
+    images_2_show = []
+    titles_2_show = []
+    for i in range(0, 10):
+        r = random.randint(1, 60000)
+        images_2_show.append(x_train[r])
+        titles_2_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))
 
-for i in range(0, 5):
-    r = random.randint(1, 10000)
-    images_2_show.append(x_test[r])
-    titles_2_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))
+    for i in range(0, 5):
+        r = random.randint(1, 10000)
+        images_2_show.append(x_test[r])
+        titles_2_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))
 
-show_images(images_2_show, titles_2_show)
+    show_images(images_2_show, titles_2_show)
+
+
+if __name__ == "__main__":
+    loadMNIST_main()
