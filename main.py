939552e269cec874c730e02fa17f30b61147af09
OfirGilad <ofirgila@post.bgu.ac.il>
2021-05-28 00:49:19 +0300

    4a update 4b start (in hope its ok)

--- main.py
+++ main.py
@@ -84,39 +84,31 @@ def generate_experiment():
     return G, x, y
 
 
-def ex4a(X, labels):
-    return Logistic_Regression(X, labels, w=np.ones(X.shape[1]))
+def ex4a(X, y):
+    return Logistic_Regression(X, y, w=np.zeros(X.shape[1]))
 
 
-def Logistic_Regression(X, labels, w):
-    number_of_labels = len(labels)
-    m = X.shape[1]
+def Logistic_Regression(X, y, w):
+    number_of_labels = len(y)
     c1 = []
     c2 = []
     for i in range(number_of_labels):
-        if labels[i] == 0:
-            c1.append(1)
-            c2.append(0)
-        else:
+        if y[i] == 0:
             c1.append(0)
             c2.append(1)
+        else:
+            c1.append(1)
+            c2.append(0)
     c1 = np.array([c1]).transpose()
     c2 = np.array([c2]).transpose()
 
     # Xtw = X.transpose() @ np.array([w]).transpose()
     Xtw = xtw(X, w)
-    sigmoidXtW = sigmoid(Xtw)
+    probability = sigmoid(Xtw)
 
-    Objective_Fw = c1.transpose() @ np.log(sigmoidXtW) + c2.transpose() @ np.log(1 - sigmoidXtW)
-    Objective_Fw = (-1 / m) * Objective_Fw
-
-    Gradient_Fw = X @ (sigmoidXtW - c1)
-    Gradient_Fw = (1 / m) * Gradient_Fw
-
-    D = np.asarray(sigmoidXtW.transpose())[0]
-    D = np.diag(D)
-    Hessian_Fw = X @ D @ X.transpose()
-    Hessian_Fw = (1 / m) * Hessian_Fw
+    Objective_Fw = Objective(X, c1, c2, probability)
+    Gradient_Fw = Gradient(X, c1, probability)
+    Hessian_Fw = Hessian(X, probability)
 
     # Fw = 0
     # for i in range(m):
@@ -135,6 +127,7 @@ def Logistic_Regression(X, labels, w):
     # xtw = np.transpose(X[:, i:i]) @ w
     # Hessian_Fw[i][i] = sigmoid(xtw) * (1 - sigmoid(xtw))
     # Hessian_Fw = 1 / m * (X @ Gradient_Fw @ X.transpose())
+    print("4a")
     print(Objective_Fw)
     print(Gradient_Fw)
     print(Hessian_Fw)
@@ -142,6 +135,7 @@ def Logistic_Regression(X, labels, w):
 
 
 def xtw(X, w):
+    # return np.dot(X.transpose(), w)
     m = X.shape[1]
     xtw_result = np.zeros([m, 1])
     for i in range(m):
@@ -150,19 +144,58 @@ def xtw(X, w):
 
 
 def sigmoid(Xtw):
-    sigmoid_result = np.exp(-Xtw)
-    sigmoid_result = 1 + sigmoid_result
-    return 1 / sigmoid_result
+    return 1 / (1 + np.exp(-Xtw))
 
 
-def ex4b(w):
+def Objective(X, c1, c2, probability):
+    m = X.shape[1]
+    # Objective_Fw = c1.transpose() @ np.log(probability) + c2.transpose() @ np.log(1 - probability)
+    return (-1 / m) * (c1.transpose() @ np.log(probability) + c2.transpose() @ np.log(1 - probability))
+
+
+def Gradient(X, c1, probability):
+    m = X.shape[1]
+    # Gradient_Fw = X @ (probability - c1)
+    return (1 / m) * (X @ (probability - c1))
+
+
+def Hessian(X, probability):
+    m = X.shape[1]
+    D = np.asarray(probability.transpose())[0]
+    D = np.diag(D)
+    # Hessian_Fw = X @ D @ X.transpose()
+    return (1 / m) * (X @ D @ X.transpose())
+
+
+def ex4b(X, y, w, epsilon):
+    d = np.random.rand(1, X.shape[1])
+    Xtw = xtw(X, w)
+    probability = sigmoid(Xtw)
+    X_ed = (X + (epsilon * d))
+    X_edtw = xtw(X_ed, w)
+    probability_new = sigmoid(X_edtw)
+    Fx_ed = Objective(X_ed, y, 1 - y, probability_new)
+    Fx = Objective(X, y, 1 - y, probability)
+    Fx_Gradient = Gradient(X, y, probability)
+    Fx_Hessian = Hessian(X, probability)
+    print("4b")
+    print(d)
+    print(Fx_ed - Fx)
+    print(np.abs(Fx_ed - Fx - (Fx_Gradient.transpose() @ (epsilon * d.transpose()))))
+    print(np.linalg.norm(Fx_ed - Fx - ((epsilon * d) @ Fx_Hessian @ (epsilon * d.transpose()))))
     return True
 
 
 if __name__ == '__main__':
     x = np.array([[2, 1, 1],
-                  [2, 1, 1],
-                  [2, 1, 1]])
+                  [2, 1, 2],
+                  [1, 1, 2]])
     y = [1, 0, 1]
-    w = [1, 1, 1]
+    w = [0.5, 0.5, 0.5]
     ex4a(x, y)
+
+    #x = np.array([[1],
+    #              [2],
+    #              [1]])
+    y = np.array([[1, 0, 1]]).transpose()
+    ex4b(x, y, w, 0.1)
