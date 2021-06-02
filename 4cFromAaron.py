fc2b614dd2a020033788060f737325564690e244
OfirGilad <ofirgila@post.bgu.ac.il>
2021-06-02 14:03:41 +0300

    update

--- 4cFromAaron.py
+++ 4cFromAaron.py
@@ -6,32 +6,20 @@ from mlxtend.data import loadlocal_mnist
 from itertools import tee
 import math
 import matplotlib.pyplot as plt
-
-
-def sigm(p):
-    return 1 / (1 + np.exp(-p))
+import main as myMain
 
 
 def get_objective(x, c, w, sigmxtw='None'):
     if (sigmxtw == 'None'):
-        xw = x @ w
-        sigmxtw = sigm(xw)
-    return -(c[0].transpose() @ np.log(sigmxtw) + c[1].transpose() @ np.log(1 - sigmxtw)) / x.shape[0]
+        sigmxtw = myMain.sigmoid(myMain.net_input(x, w))
+    return -(c.transpose() @ np.log(sigmxtw) + (1 - c.transpose()) @ np.log(1 - sigmxtw)) / x.shape[0]
 
 
-def compute_logit(x, c, w, compute_hessian=True):
-    # X is of the format [index x datavec (1 x n)]
-    # C is of format [index x label_vector(1 x m)]
-    xw = x @ w
-    sigmxtw = sigm(xw)
-    objective = get_objective(x, c, w, sigmxtw)
-    deriv = (1 / x.shape[0]) * x.transpose() @ (sigmxtw - c[0])
-    # Making sure that derivative is in same direction but way smaller
-    # if(np.linalg.norm(deriv) > 1):
-    # deriv = 1 * deriv / np.linalg.norm(deriv)
-
+def compute_logit(x, y, w, compute_hessian=True):
+    objective = myMain.cost(w, x, y)
+    deriv = myMain.gradient(w, x, y)
     if (compute_hessian):
-        hessian = (1 / x.shape[0]) * np.multiply(x.transpose(), sigmxtw * (1 - sigmxtw)) @ x
+        hessian = myMain.hessian(w, x, y)
         return (objective, deriv, hessian)
     else:
         return (objective, deriv)  # +end_src
@@ -43,11 +31,11 @@ x = np.array([[1, 2],
               [3, 4],
               [5, 6],
               [7, 8],
-              [-1, 2]]).transpose()
+              [-1, 2]])
 x = x.transpose()
-c = np.array([[0, 0, 1, 0, 1], [1, 0, 0, 0, 0], [0, 1, 0, 1, 0]])
-w = np.array([1, 2])
-objective, deriv, hessian = compute_logit(x, c, w)
+y = np.array([[0, 0, 1, 0, 1]]).transpose()
+w = np.array([[1, 2]]).transpose()
+objective, deriv, hessian = compute_logit(x, y, w)
 print(hessian)
 
 cwd = os.getcwd()
@@ -66,36 +54,36 @@ xtest = xtest / np.max(xtest)
 def filter_data_for_digits(digita, digitb):
     train_indeces = (i for i in range(xtrain.shape[0]) if ytrain[i] in [digita, digitb])
     ti1, ti2 = tee(train_indeces)
-    xtr = np.array([xtrain[i] for i in ti1])
+    xtr = np.array([xtrain[i] for i in ti1]).transpose()
     ytr = np.array([ytrain[i] for i in ti2])
+    y_train = np.array([ytr]).transpose()
     test_indeces = (i for i in range(xtest.shape[0]) if ytest[i] in [digita, digitb])
     ti1, ti2 = tee(test_indeces)
-    xte = np.array([xtest[i] for i in ti1])
+    xte = np.array([xtest[i] for i in ti1]).transpose()
     yte = np.array([ytest[i] for i in ti2])
-    ctr = np.array([[1 if y == i else 0 for y in ytr] for i in np.unique(ytr)])
-    cte = np.array([[1 if y == i else 0 for y in yte] for i in np.unique(yte)])
-
-    # Return new x/y train, x/y test, c train, c test.
-    return (xtr, ytr, xte, yte, ctr, cte)
+    y_test = np.array([yte]).transpose()
+    return (xtr, y_train, xte, y_test)
 
 
 def correct_frac(digita, digitb, x, y, w):
     # Returns percentage of correct classifications
-    correct_0 = len([i for i in range(len(x)) if (y[i] == digita) and (sigm(x[i] @ w) > 0.5)])
-    correct_1 = len([i for i in range(len(x)) if (y[i] == digitb) and (sigm(x[i] @ w) <= 0.5)])
+    y = y.transpose()[0]
+    x = x.transpose()
+    correct_0 = len([i for i in range(len(x)) if (y[i] == digita) and (myMain.sigmoid(x[i] @ w) > 0.5)])
+    correct_1 = len([i for i in range(len(x)) if (y[i] == digitb) and (myMain.sigmoid(x[i] @ w) <= 0.5)])
     correct = correct_0 + correct_1
     return float(correct) / len(x)
 
 
-def get_alpha_armijo(x, c, w, d, deriv):
+def get_alpha_armijo(x, y, w, d, deriv):
     # Function performs line sarch according to armijo, retrns alpha
     pow = 0
     alpha = 0.8
-    baseobj = get_objective(x, c, w)
+    baseobj = get_objective(x, y, w)
 
     while (True):
         newalpha = math.pow(alpha, pow)
-        leftop = get_objective(x, c, w + newalpha * d)
+        leftop = get_objective(x, y, w + newalpha * d)
         rightop = baseobj + 0.00001 * newalpha * (d.transpose() @ deriv)
 
         if leftop < rightop:
@@ -104,8 +92,8 @@ def get_alpha_armijo(x, c, w, d, deriv):
             pow += 1
 
 
-def train(digita, digitb, xtrain, xtest, ctrain, ctest, sd=True):
-    w = np.zeros(xtrain.shape[1])
+def train(digita, digitb, xtrain, xtest, ytrain, ytest, sd=True):
+    w = np.zeros((xtrain.shape[0], 1))
     oldobj = 0
     trainobjs = []
     testobjs = []
@@ -122,7 +110,7 @@ def train(digita, digitb, xtrain, xtest, ctrain, ctest, sd=True):
         # On first time, don't run armijo
         if (not init):
             # Running armijo
-            alpha = get_alpha_armijo(xtrain, ctrain, w, d, deriv)
+            alpha = get_alpha_armijo(xtrain, ytrain, w, d, deriv)
         else:
             alpha = 1
             init = False
@@ -136,33 +124,38 @@ def train(digita, digitb, xtrain, xtest, ctrain, ctest, sd=True):
         iter += 1
 
         if (sd):
-            obj, deriv = compute_logit(xtrain, ctrain, w, False)
+            obj, deriv = compute_logit(xtrain, ytrain, w, False)
             # Steepest descent
             d = -deriv
 
         else:
-            obj, deriv, hes = compute_logit(xtrain, ctrain, w)
+            obj, deriv, hes = compute_logit(xtrain, ytrain, w)
             # Calculating according to newton
             d = -np.linalg.inv(hes + np.identity(hes.shape[0]) * 0.01) @ deriv
 
-        trainobjs.append(obj)
-        testobjs.append(get_objective(xtest, ctest, w))
+        trainobjs.append(obj[0][0])
+        testobjs.append(myMain.cost(w, xtest, ytest)[0][0])
 
     return (w, np.array(trainobjs), np.array(testobjs))
 
 
 def train_digits(digita, digitb, sd=True):
-    xtr, ytr, xte, yte, ctr, cte = filter_data_for_digits(digita, digitb)
-    w, trainhistory, testhistory = train(digita, digitb, xtr, xte, ctr, cte, sd)
+    xtr, ytr, xte, yte = filter_data_for_digits(digita, digitb)
+    w, trainhistory, testhistory = train(digita, digitb, xtr, xte, 1-ytr, 1-yte, sd)
+    print(trainhistory)
     cfrac = correct_frac(digita, digitb, xte, yte, w)
     return (np.abs(trainhistory - trainhistory[-1]), np.abs(testhistory - testhistory[-1]), cfrac, testhistory[-1], w)
 
 
 def main():
     sd01 = train_digits(0, 1, sd=True)
+    print("PASS")
     nw01 = train_digits(0, 1, sd=False)
+    print("PASS")
     sd89 = train_digits(8, 9, sd=True)
+    print("PASS")
     nw89 = train_digits(8, 9, sd=False)
+    print("PASS")
     fig, axs = plt.subplots(2, figsize=(9, 8))
     axs[0].plot(sd01[0], color='blue')
     axs[0].plot(nw01[0], color='red')
